a95715bdc557018af6d86df8d120fb113a39975a
Mohmed_Yousry <53487925+mohmed-yousry@users.noreply.github.com>
2022-10-27 13:51:00 +0200

    Update public.js

--- js/public.js
+++ js/public.js
@@ -2,7 +2,7 @@ console.log(window.location.href);
 
 // تسجيل السيرفس ووركر
 if(navigator.serviceWorker) {
-  navigator.serviceWorker.register("../matebos/sw.js") ;
+  navigator.serviceWorker.register("sw.js") ;
 }
 
 // تسجيل السيرفس ووركر
